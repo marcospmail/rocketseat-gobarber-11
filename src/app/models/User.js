cef2d96005c846f8115930f2792fa5c839f4cabd
Marcos Paulo <marcosp.mail@gmail.com>
2019-10-17 16:25:07 -0300

    Implements Files table and references its id in Users table

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -22,6 +22,10 @@ class User extends Model {
     });
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -15,6 +16,9 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
