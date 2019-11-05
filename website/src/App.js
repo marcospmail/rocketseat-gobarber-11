fef8db473acd31414acff6ff50d1b2caa8b55aca
Marcos Paulo <marcosp.mail@gmail.com>
2019-11-05 17:27:15 -0300

    Implements SignIn and SignUp pages

--- website/src/App.js
+++ website/src/App.js
@@ -7,10 +7,13 @@ import Routes from './routes';
 
 import history from './services/history';
 
+import GlobalStyle from './styles/global'
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
