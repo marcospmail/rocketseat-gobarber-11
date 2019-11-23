bd97280ba17056d954aef13720d24e28a6625cc7
Marcos Paulo <marcosp.mail@gmail.com>
2019-11-22 22:13:24 -0300

    chore: Order providers filter by name

--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -5,6 +5,7 @@ class ProviderController {
   async index(req, res) {
     const providers = await Provider.findAll({
       where: { provider: true },
+      order: ['name'],
       attributes: ['id', 'name', 'email', 'avatar_id'],
       include: {
         model: File,
